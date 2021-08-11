9f7454553b4c220d2252c245c087e48a80d3243d
unknown <superhbk@naver.com>
2021-08-11 17:01:23 +0900

    react-study

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
 import React from 'react';
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Navigation from './components/Navigation';
+import "./App.css";
 
-
-class App extends React.Component{
-  state = {
-    count: 0
-  };
-  add= () => {
-    this.setState(current =>({count: current.count +1}));
-  };
-  minus= () => {
-    this.setState(current =>({count: current.count -1}));
-  };
-  componentDidMount(){
-    console.log("component rendered")
-  }
-  componentDidUpdate(){
-    console.log("I'm just updated")
-  }
-  render(){
-    console.log("I'm rendering")
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
-  }
+function App(){
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home}/>
+      <Route path="/about" component={About}/>
+    </HashRouter>
+  )
 }
 
 export default App;
